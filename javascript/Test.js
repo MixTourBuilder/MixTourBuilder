31fe6ed0f02c1f6176ecae1965bdec52fd3dbbfd
Benoit <Brimeux.Benoit@gmail.com>
2015-12-07 14:27:36 +0100

    Test init_joueur

--- javascript/Test.js
+++ javascript/Test.js
@@ -5,3 +5,16 @@ projectEngineTestCase.prototype.testInitPlateau = function () {
     e.init_plateau();
     assertTrue(e.get_nb_piece_plateau == 0);
 };
+
+projectEngineTestCase.prototype.testInitJoueur = function () {
+    var e = new Engine();
+    e.initBoard();
+    assertEquals(e.get_pion_joueur(0), 25);
+    assertEquals(e.get_pion_joueur(1), 25);
+    assertEquals(e.get_score_joueur(0), 0);
+    assertEquals(e.get_score_joueur(1), 0);
+    assertEquals(e.get_joueur_courant(), 0);
+    e.switch_joueur();
+    assertEquals(e.get_joueur_courant(), 1);
+};
+
