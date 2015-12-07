a8f569e78935c7fb003f1e91a998d12c84b853da
Benoit <Brimeux.Benoit@gmail.com>
2015-12-07 13:52:13 +0100

    Test InitPlateau

--- javascript/Test.js
+++ javascript/Test.js
@@ -1,3 +1,7 @@
 var projectEngineTestCase = TestCase("projectEngineTestCase");
 
-
+projectEngineTestCase.prototype.testInitPlateau = function () {
+    var e = new Engine();
+    e.initBoard();
+    assertTrue(e.getNbPiece == 0);
+};
